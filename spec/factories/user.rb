c8283e5548c6b4ed7e2097e28c99d10d2787eb55
Rafael V. dos Santos <rvsantos00@gmail.com>
2019-10-11 22:11:53 -0300

    Improving factorie user

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -3,5 +3,16 @@ FactoryBot.define do
     email { Faker::Internet.email }
     password { 123456 }
     password_confirmation { 123456 }
+
+    trait :without_email do
+      email { 'invalid_email@' }
+    end
+
+    trait :without_password do
+      password nil
+    end
+
+    factory :user_invalid_email, traits: [:without_email]
+    factory :user_without_password, traits: [:without_password]
   end
 end
