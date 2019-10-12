d986e28c50541aebe35618af5776025c4d2e2572
Rafael V. dos Santos <rvsantos00@gmail.com>
2019-10-12 00:22:15 -0300

    Adding auth_token and #info to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :auth_token, uniqueness: true
+
+  def info
+    "#{email} - #{created_at}"
+  end
 end
